7d3854b61744d8b108481eb73430c76c2473a9cb
Amitgit1st <aamitp6297@gmail.com>
2023-05-18 10:20:30 +0530

    ObjectinLocalStorage

---
+++ objectstorage.js
@@ -0,0 +1,8 @@
+let myObj ={
+    name: "AMIT",
+    age : 28
+};
+let myObj_serialized = JSON.stringify(myObj);
+localStorage.setItem("myObj",myObj_serialized);
+let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+console.log(myObj_deserialized);
